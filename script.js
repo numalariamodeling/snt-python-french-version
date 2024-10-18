62c3a6f103b6e504142901a89342cfb9fb29f815
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-10-18 11:49:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ function loadContent(page) {
 
 
             <div class="round-buttons">
-                <button class="rect-button" onclick="window.location.href='https://';">Vue R FR</button>
-                <button class="rect-button" onclick="window.location.href='https://';">Vue py EN</button>
-                <button class="rect-button" onclick="window.location.href='https://';">Vue R En</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-library-french-version/#shapefiles';">Vue R FR</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-python-english-version/#shapefiles';">Vue py EN</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-library-english-version/#shapefiles';">Vue R En</button>
 
             </div>
            
